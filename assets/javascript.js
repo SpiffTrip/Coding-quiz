7b1c34a361d32814b0d5715bd7c5690bbd6bd9f7
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-01-20 15:59:00 -0700

    added framework

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,49 @@
-question1 ="What does 'CDN' stand for?"
-question2 ="What does 'DOM' stand for?"
-question3 ="What does 'HTML' stand for?"
+//question1 ="What does 'CDN' stand for?";
+//question2 ="What does 'DOM' stand for?";
+//question3 ="What does 'HTML' stand for?";
+
+var question1 = {
+    title: "what does 'CDN' stand for?",
+    answers: ["Content Delivery Network", "Can't Denote Network","Character Design Notebook", "Cats Dogs Nothing"],
+    correctAnswer: 0
+};
+
+var question2 = {
+    title: "What does 'DOM' stand for",
+    answers: ["Document On Main","Document Object Model","Double Object Manipulation","Dogs On Moms"],
+    correctAnswer: 1
+};
+
+var question3 = {
+    title: "What does 'HTML' stand for?",
+    answers: ["Huge Time Many Language","How Text Makes Language","Hot Turtles Making Lemonaide","Hyper Text Markup Language"],
+    correctAnswer: 3
+};
+
+function showQuestion() {
+
+    var titleDiv = document.getElementById("title");
+titleDiv.textContent = question1.title;
+
+  var answ = document.querySelectorAll(".answers")
+  answ.forEach(function(element,index){
+      element.textContent = question1.answers[index];
+
+      element.addEventListener("click",function(){
+          if(question1.correctAnswer == index) {
+              console.log("correct answer");
+          }
+          else {
+              console.log("wrong answer");
+          }
+      });
+  });
+}
+
+showQuestion(question1);
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    console.log("clicked");
+});
